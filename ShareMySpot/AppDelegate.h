1d10c02373dc6f945afa0c3e456300307465cd93
Chris Jungmann <cjungm@gmail.com>
2014-01-13 19:52:44 -0700

    Initial Commit

---
+++ ShareMySpot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShareMySpot
+//
+//  Created by chris on 1/13/14.
+//  Copyright (c) 2014 Chris Jungmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareMySpotTests/ShareMySpotTests.m
@@ -0,0 +1,34 @@
+//
+//  ShareMySpotTests.m
+//  ShareMySpotTests
+//
+//  Created by chris on 1/13/14.
+//  Copyright (c) 2014 Chris Jungmann. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShareMySpotTests : XCTestCase
+
+@end
+
+@implementation ShareMySpotTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
